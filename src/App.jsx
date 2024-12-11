492be47073be92a12cd267361f631dcaee923a32
MennaKewan <mennaakewan@gmail.com>
2024-12-11 23:31:51 +0200

    Save local changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Footer from './components/Footer/Footer';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Menu from './pages/Menu/Menu';
-import Gallery from './pages/Gallery/Gallery';
 import Contact from './pages/Contact/Contact';
 import Login from './pages/1-Login/Login';
 
@@ -14,10 +13,9 @@ function App() {
     <div > 
       <Navbar /> 
       <Routes>
-       <Route path="/home" element={<Home />} />
+      <Route path="/home" element={<Home />} />
        <Route path="/about" element={<About />} />
        <Route path="/menu" element={<Menu />} />
-       <Route path="/gallery" element={<Gallery />} />
        <Route path="/contact" element={<Contact />} />
        <Route path="/login" element={<Login />} />
 
